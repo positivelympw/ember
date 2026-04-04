22840c1377d3afc9f3177309efcd00283016d512
WatchWeWin <explore@watchwewin.com>
2026-04-03 22:45:45 -0400

    Initial Commit

---
+++ Ember/EmberApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberApp.swift
+//  Ember
+//
+//  Created by Marcus Win on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EmberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmberTests/EmberTests.swift
@@ -0,0 +1,18 @@
+//
+//  EmberTests.swift
+//  EmberTests
+//
+//  Created by Marcus Win on 4/3/26.
+//
+
+import Testing
+
+struct EmberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
